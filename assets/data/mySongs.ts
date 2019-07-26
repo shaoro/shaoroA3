0ea5805f8feda2fae3f5f3f1a63357f94b03d4d8
shaoro <52579116+shaoro@users.noreply.github.com>
2019-07-26 14:21:39 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,11 @@
+import { SongInfo } from '../../app/songInfo';
+
+export const MYSONGS: SongInfo [] = [
+  {songname: 'Butter-Fly', artist: 'Wada Koji', genre: 'J-pop, anison', yearReleased: 'April 23, 1999', picture: './assets/image/Butter-Fly.jpg'},
+  {songname: 'Akatsuki no Kuruma(暁の車)', artist: 'FictionJunction YUUKA', genre:'J-pop', yearReleased: 'September 22, 2004', picture: './assets/image/Seed.jpg'},
+  {songname: 'For Fruits Basket(For フルーツバスケット)', artist: 'Ritsuko Okazaki', genre: 'J-pop', yearReleased: 'July 25, 2001', picture: './assets/image/FruitBasket.jpg'},
+  {songname: '世界が终るまでは…', artist: 'WANDS', genre: 'J-pop', yearReleased: 'June 8, 1994', picture: './assets/image/TillTheEnd.jpeg'}
+]
+
+
+
---
+++ assets/data/studentName.ts
@@ -0,0 +1,5 @@
+import { Shaoro } from '../../app/shaoro';
+
+export const STUDENTNAME: Shaoro = {
+  sid: 991436294, sname: 'Rong Shao', slogin: 'shaoro', scampus: 'Trafalgar', stitle: 'Assignment 03'
+}
